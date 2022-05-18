a8cb38842645ef9f6fe225269e6d16b1da0de504
tobygal <tochiazolike@yahoo.com>
2022-05-18 08:33:13 -0700

    Update util.h :bug:

--- util.h
+++ util.h
@@ -2,7 +2,9 @@
 #define _UTIL_H
 
 #include <unistd.h>
+#include <limits.h>
 #include "_string.h"
+#include "shell.h"
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 char **_realloc2(char **ptr, unsigned int old_size, unsigned int new_size);
