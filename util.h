da2eff5b10f9a7a6155e290eea61b29567e7edef
tobygal <tochiazolike@yahoo.com>
2022-05-18 07:31:28 -0700

    Add error_message function :art:

--- util.h
+++ util.h
@@ -6,5 +6,6 @@
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 char **_realloc2(char **ptr, unsigned int old_size, unsigned int new_size);
+void error_message(shell_t *shell)
 
 #endif
